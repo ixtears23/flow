ab829d3b703d368a47a83c7b39e4ce628343e328
JunseokOh <ixtears23@icloud.com>
2024-01-29 01:30:11 +0900

    서비스 관련 테스트 코드 추가

--- src/test/java/junseok/snr/flow/service/UserQueueServiceTest.java
+++ src/test/java/junseok/snr/flow/service/UserQueueServiceTest.java
@@ -57,10 +57,68 @@ class UserQueueServiceTest {
     }
 
     @Test
-    void allowUser() {
+    void emptyAllowUser() {
+        StepVerifier.create(userQueueService.allowUser("default", 3L))
+                .expectNext(0L)
+                .verifyComplete();
     }
 
     @Test
+    void allowUser() {
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100L)
+                        .then(userQueueService.registerWaitQueue("default", 101L))
+                        .then(userQueueService.registerWaitQueue("default", 102L))
+                        .then(userQueueService.allowUser("default", 3L)))
+                .expectNext(2L)
+                .verifyComplete();
+    }
+
+
+    @Test
+    void allowUser2() {
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100L)
+                        .then(userQueueService.registerWaitQueue("default", 101L))
+                        .then(userQueueService.registerWaitQueue("default", 102L))
+                        .then(userQueueService.allowUser("default", 5L)))
+                .expectNext(3L)
+                .verifyComplete();
+    }
+
+
+    @Test
+    void allowUserAfterRegisterWaitQueue() {
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100L)
+                        .then(userQueueService.registerWaitQueue("default", 101L))
+                        .then(userQueueService.registerWaitQueue("default", 102L))
+                        .then(userQueueService.allowUser("default", 3L))
+                        .then(userQueueService.registerWaitQueue("default", 200L)))
+                .expectNext(1L)
+                .verifyComplete();
+    }
+
+    @Test
+    void isNotAllowed() {
+        StepVerifier.create(userQueueService.isAllowed("default", 100L))
+                .expectNext(false)
+                .verifyComplete();
+    }
+
+    @Test
+    void isNotAllowed2() {
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100L)
+                        .then(userQueueService.allowUser("default", 3L))
+                        .then(userQueueService.isAllowed("default", 101L)))
+                .expectNext(false)
+                .verifyComplete();
+    }
+
+
+    @Test
     void isAllowed() {
+        StepVerifier.create(userQueueService.registerWaitQueue("default", 100L)
+                        .then(userQueueService.allowUser("default", 3L))
+                        .then(userQueueService.isAllowed("default", 100L)))
+                .expectNext(true)
+                .verifyComplete();
     }
 }
