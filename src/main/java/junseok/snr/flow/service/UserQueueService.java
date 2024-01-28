120914b4ec0f6e269ab65a9a758eaaa618599f8a
JunseokOh <ixtears23@icloud.com>
2024-01-29 00:51:25 +0900

    application exception 추가

--- src/main/java/junseok/snr/flow/service/UserQueueService.java
+++ src/main/java/junseok/snr/flow/service/UserQueueService.java
@@ -1,5 +1,6 @@
 package junseok.snr.flow.service;
 
+import junseok.snr.flow.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -22,7 +23,7 @@ public class UserQueueService {
         var unixTimestamp = Instant.now().getEpochSecond();
         return reactiveRedisTemplate.opsForZSet().add(USER_QUEUE_WAIT_KEY.formatted(queue), userId.toString(), unixTimestamp)
                 .filter(i -> i)
-                .switchIfEmpty(Mono.error(new Exception("already register user...")))
+                .switchIfEmpty(Mono.error(ErrorCode.QUEUE_ALREADY_REGISTER_USER.build()))
                 .flatMap(i -> reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_WAIT_KEY.formatted(queue), userId.toString()))
                 .map(i -> i >=0 ? i + 1 : i);
     }
