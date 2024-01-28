90ea837bb126795d686743c66d66e6179f2e468f
JunseokOh <ixtears23@icloud.com>
2024-01-29 00:34:13 +0900

    대기열 등록 API 추가

---
+++ src/main/java/junseok/snr/flow/service/UserQueueService.java
@@ -0,0 +1,29 @@
+package junseok.snr.flow.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.time.Instant;
+
+@Service
+@RequiredArgsConstructor
+public class UserQueueService {
+    private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
+
+    // 대기열 등록 API
+    public Mono<Long> registerWaitQueue(final Long userId) {
+        // redis sortedset
+        // - key : userId
+        // - value : unix timestamp
+
+        var unixTimestamp = Instant.now().getEpochSecond();
+        return reactiveRedisTemplate.opsForZSet().add("user-queue", userId.toString(), unixTimestamp)
+                .filter(i -> i)
+                .switchIfEmpty(Mono.error(new Exception("already register user...")))
+                .flatMap(i -> reactiveRedisTemplate.opsForZSet().rank("user-queue", userId.toString()))
+                .map(i -> i >=0 ? i + 1 : i);
+    }
+
+}
