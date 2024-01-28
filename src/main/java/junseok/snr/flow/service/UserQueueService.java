3170284d934d1af51f5efc589d28171665fdde1d
JunseokOh <ixtears23@icloud.com>
2024-01-29 02:15:11 +0900

    대기열 스케줄러 추가

--- src/main/java/junseok/snr/flow/service/UserQueueService.java
+++ src/main/java/junseok/snr/flow/service/UserQueueService.java
@@ -2,18 +2,27 @@ package junseok.snr.flow.service;
 
 import junseok.snr.flow.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuples;
 
 import java.time.Instant;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserQueueService {
     private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
     private static final String USER_QUEUE_WAIT_KEY = "users:queue:%s:wait";
+    private static final String USER_QUEUE_WAIT_KEY_FOR_SCAN = "users:queue:*:wait";
     private static final String USER_QUEUE_PROCEED_KEY = "users:queue:%s:proceed";
+    @Value("${scheduler.enabled}")
+    private Boolean scheduled = false;
 
     // 대기열 등록 API
     public Mono<Long> registerWaitQueue(final String queue, final Long userId) {
@@ -51,6 +60,27 @@ public class UserQueueService {
         return reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_WAIT_KEY.formatted(queue), userId.toString())
                 .defaultIfEmpty(-1L)
                 .map(rank -> rank >= 0 ? rank + 1 : rank);
+    }
+
+    @Scheduled(initialDelay = 5000, fixedDelay = 10000)
+    public void scheduleAllowUser() {
+        if (!scheduled) {
+            log.info("passed scheduling...");
+            return;
+        }
+
+        log.info("called scheduling...");
+
+        var maxAllowUserCount = 3L;
+
+        reactiveRedisTemplate.scan(ScanOptions.scanOptions()
+                        .match(USER_QUEUE_WAIT_KEY_FOR_SCAN)
+                        .count(100)
+                        .build())
+                .map(key -> key.split(":")[2])
+                .flatMap(queue -> allowUser(queue, maxAllowUserCount).map(allowed -> Tuples.of(queue, allowed)))
+                .doOnNext(tuple -> log.info("Tried %d and allowed %d members of %s queue".formatted(maxAllowUserCount, tuple.getT2(), tuple.getT1())))
+                .subscribe();
     }
 
 }
